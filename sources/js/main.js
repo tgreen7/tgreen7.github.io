d7886216a9802949a769534466bb0a9ad5b61f9d
Taoh Green <greenmachine777@gmail.com>
2016-08-23 23:28:15 -0700

    isotope filtering works but page jumps

--- sources/js/main.js
+++ sources/js/main.js
@@ -2,46 +2,6 @@ jQuery(function($){
 
 var BRUSHED = window.BRUSHED || {};
 
-/* ==================================================
-   Mobile Navigation
-================================================== */
-var mobileMenuClone = $('#menu').clone().attr('id', 'navigation-mobile');
-
-BRUSHED.mobileNav = function(){
-    var windowWidth = $(window).width();
-    
-    if( windowWidth <= 979 ) {
-        if( $('#mobile-nav').length > 0 ) {
-            mobileMenuClone.insertAfter('#menu');
-            $('#navigation-mobile #menu-nav').attr('id', 'menu-nav-mobile');
-        }
-    } else {
-        $('#navigation-mobile').css('display', 'none');
-        if ($('#mobile-nav').hasClass('open')) {
-            $('#mobile-nav').removeClass('open');   
-        }
-    }
-}
-
-BRUSHED.listenerMenu = function(){
-    $('#mobile-nav').on('click', function(e){
-        $(this).toggleClass('open');
-        
-        if ($('#mobile-nav').hasClass('open')) {
-            $('#navigation-mobile').slideDown(500, 'easeOutExpo');
-        } else {
-            $('#navigation-mobile').slideUp(500, 'easeOutExpo');
-        }
-        e.preventDefault();
-    });
-    
-    $('#menu-nav-mobile a').on('click', function(){
-        $('#mobile-nav').removeClass('open');
-        $('#navigation-mobile').slideUp(350, 'easeOutExpo');
-    });
-}
-
-
 
 /* ==================================================
    Navigation Fix
@@ -57,50 +17,40 @@ BRUSHED.nav = function(){
 ================================================== */
 
 BRUSHED.filter = function (){
-    if($('#projects').length > 0){      
-        var $container = $('#projects');
-        
-        $container.imagesLoaded(function() {
-            $container.isotope({
-              // options
-              animationEngine: 'best-available',
-              itemSelector : '.item-thumbs',
-              layoutMode : 'fitRows'
+    if($('#projects').length > 0) {  
+
+        var container = $('#projects');
+
+        container.imagesLoaded( function() {
+            container.isotope({
+                itemSelector: '.item-thumbs',
+                layoutMode: 'fitRows',
+                animationEngine: 'best-available'
             });
-        });
-    
-        
+        })
+
         // filter items when filter link is clicked
-        var $optionSets = $('#options .option-set'),
-            $optionLinks = $optionSets.find('a');
-    
-          $optionLinks.click(function(){
-            var $this = $(this);
+        var optionSets = $('#options .option-set');
+        var optionLinks = optionSets.find('a');
+
+
+        optionLinks.click(function () {
+            var link = $(this);
             // don't proceed if already selected
-            if ( $this.hasClass('selected') ) {
+            if ( link.hasClass('selected') ) {
               return false;
             }
-            var $optionSet = $this.parents('.option-set');
-            $optionSet.find('.selected').removeClass('selected');
-            $this.addClass('selected');
-      
-            // make option object dynamically, i.e. { filter: '.my-filter-class' }
-            var options = {},
-                key = $optionSet.attr('data-option-key'),
-                value = $this.attr('data-option-value');
-            // parse 'false' as false boolean
-            value = value === 'false' ? false : value;
-            options[ key ] = value;
-            if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-              // changes in layout modes need extra logic
-              changeLayoutMode( $this, options )
-            } else {
-              // otherwise, apply new options
-              $container.isotope( options );
-            }
-            
-            return false;
+            var optionSet = link.parents('.option-set');
+            optionSet.find('.selected').removeClass('selected');
+            link.addClass('selected');
+            var filterValue = link.attr('data-filter')
+
+
+            container.isotope({ filter: filterValue });
         });
+
+        return false;
+
     }
 }
 
@@ -303,8 +253,6 @@ $(document).ready(function(){
     });
     
     BRUSHED.nav();
-    BRUSHED.mobileNav();
-    BRUSHED.listenerMenu();
     BRUSHED.menu();
     BRUSHED.goSection();
     BRUSHED.goUp();
@@ -318,8 +266,5 @@ $(document).ready(function(){
 
 });
 
-$(window).resize(function(){
-    BRUSHED.mobileNav();
-});
 
 });
